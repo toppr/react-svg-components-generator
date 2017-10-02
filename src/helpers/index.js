5c5e2d4ee76e0400a10e6186cdfddde56176dfd9
Gautam Chaudhary <gkcgautam@gmail.com>
2017-10-02 20:33:32 +0530

    Make necessary eslint warning fixes

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,11 +1,21 @@
+const buildComponentPath = require('./buildComponentPath');
+const camelizeElementAttributes = require('./camelizeElementAttributes');
+const capitalizeComponentPath = require('./capitalizeComponentPath');
+const createDomElement = require('./createDomElement');
+const createIndexFile = require('./createIndexFile');
+const getComponentNameFromPath = require('./getComponentNameFromPath');
+const optimizeSvg = require('./optimizeSvg');
+const removeExtensionFromFilePath = require('./removeExtensionFromFilePath');
+const setSvgElementColorAttributeValue = require('./setSvgElementColorAttributeValue');
+
 module.exports = {
-    buildComponentPath: require('./buildComponentPath'),
-    camelizeElementAttributes: require('./camelizeElementAttributes'),
-    capitalizeComponentPath: require('./capitalizeComponentPath'),
-    createDomElement: require('./createDomElement'),
-    createIndexFile: require('./createIndexFile'),
-    getComponentNameFromPath: require('./getComponentNameFromPath'),
-    optimizeSvg: require('./optimizeSvg'),
-    removeExtensionFromFilePath: require('./removeExtensionFromFilePath'),
-    setSvgElementColorAttributeValue: require('./setSvgElementColorAttributeValue'),
+    buildComponentPath,
+    camelizeElementAttributes,
+    capitalizeComponentPath,
+    createDomElement,
+    createIndexFile,
+    getComponentNameFromPath,
+    optimizeSvg,
+    removeExtensionFromFilePath,
+    setSvgElementColorAttributeValue,
 };
